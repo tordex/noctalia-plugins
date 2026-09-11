c86bd0c1c49be4353e0c423a072953d98b9c982c
Yuri Kobets <yuri.kobets@gmail.com>
2026-09-11 03:11:52 +0300

    procs: show system processes, workaround the 256 children limitation

--- procs/procs.py
+++ procs/procs.py
@@ -6,13 +6,13 @@
 import os
 import sys
 import errno
-import io
 import json
 import time
 import subprocess
 import gi
 import psutil
 import draw_graph
+import pwd
 
 gi.require_version('Gio', '2.0')
 gi.require_version('GioUnix', '2.0')
@@ -71,16 +71,6 @@ def icon_path(icon_name, size=24):
     default_icon_name = "application-x-executable"  # You can change this to any default icon you prefer
     return get_icon_path(default_icon_name, size)  # Return None if no icon is found
 
-
-class Unbuffered(io.TextIOBase):
-   def __init__(self, stream):
-       super().__init__()
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def close(self):
-       self.stream.close()
 
 class Proc:
     def __init__(self):
@@ -188,19 +178,14 @@ class Proc:
                 "swap": int(swap * 1024),
             }
         except (LookupError, ProcessLookupError):
+            shared = int(proc.open(pid, 'statm').readline().split()[2])
+            shared *= PAGESIZE
+            private = rss - shared
             return {
-                "private": 0,
-                "shared": 0,
-                "swap": 0,
+                "private": int(private * 1024),
+                "shared": int(shared * 1024),
+                "swap": int(swap * 1024),
             }
-            # shared = int(proc.open(pid, 'statm').readline().split()[2])
-            # shared *= PAGESIZE
-            # private = rss - shared
-            # return {
-            #     "private": int(private * 1024),
-            #     "shared": int(shared * 1024),
-            #     "swap": int(swap * 1024),
-            # }
 
     def get_cpu_ticks(self, pid: int):
         """Reads the system's total CPU time and the specific process's ticks."""
@@ -229,6 +214,16 @@ class Proc:
                 "ticks": None
             }
 
+    def get_uid(self, pid: int):
+        """Reads the user ID (UID) of the specific process."""
+        try:
+            with self.open(pid, "status") as f:
+                for line in f:
+                    if line.startswith("Uid:"):
+                        return int(line.split()[1])
+            return -1
+        except (LookupError, ProcessLookupError) as e:
+            return -1
 
     def get_ppid(self, pid: int):
         """Reads the parent process ID (PPID) of the specific process."""
@@ -398,6 +393,12 @@ def get_system_stats():
     if board is None and board_name is not None:
         board = board_name
 
+    uid = os.getuid()
+    try:
+        username = pwd.getpwuid(uid).pw_name
+    except KeyError:
+        username = "<unknown>"
+
     return {
         "cpu_percent": cpu_percent,
         "cpu_temp": cpu_temp,
@@ -408,6 +409,8 @@ def get_system_stats():
         "kernel_version": kernel_version,
         "cpu_name": cpu_name,
         "board": board,
+        "uid": uid,
+        "username": username,
     }
 
 
@@ -480,18 +483,29 @@ def fetch_processes(interval: float):
         if proc_data["private"] == 0 and proc_data["shared"] == 0 and proc_data["swap"] == 0:
             continue
 
+        uid = proc.get_uid(pid)
+        if uid == -1:
+            continue
+        try:
+            username = pwd.getpwuid(uid).pw_name
+        except KeyError:
+            username = "<unknown>"
+
         proc_data.update({
             "pid": pid,
             "ppid": ppid,
             "exe": exe,
             "name": name,
             "comm": comm,
+            "uid": uid,
+            "username": username,
             "icon": icon_path(name, 24),
             "cmdline": cmdline,
             "mem": proc_data["private"] + proc_data["shared"],
             "cpu": round(cpu_usage_percent, 1),
             "io_read": int(delta_read_b / interval),
-            "io_write": int(delta_write_b / interval)
+            "io_write": int(delta_write_b / interval),
+            "io": int((delta_read_b + delta_write_b) / interval)
         })
         processes[pid] = proc_data
     return processes
